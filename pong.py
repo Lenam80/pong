66a0537149bd5552f1967021447be520bb7e10cc
Angelina <46305610+Lenam80@users.noreply.github.com>
2020-02-03 13:34:29 -0500

    Create pong.py

---
+++ pong.py
@@ -0,0 +1,15 @@
+# Pong Game
+
+# By Angelina
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Angelina")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
